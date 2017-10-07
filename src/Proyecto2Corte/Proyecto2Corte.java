3e26a011307403825cf003ede791d11cc8e973c6
xxxREIDENxxx <reiden-3@hotmail.com>
2017-10-07 13:00:06 -0500

    primerCommit

---
+++ src/Proyecto2Corte/Proyecto2Corte.java
@@ -0,0 +1,10 @@
+package Proyecto2Corte;
+
+public class Proyecto2Corte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Proyecto2Corte/VentanaPrincipal.java
@@ -0,0 +1,9 @@
+package Proyecto2Corte;
+
+public class VentanaPrincipal {
+
+	public VentanaPrincipal() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
