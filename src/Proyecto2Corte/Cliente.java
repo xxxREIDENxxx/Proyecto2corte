6ddfff56e129f311409aa398b537987a1bddf8e9
xxxREIDENxxx <reiden-3@hotmail.com>
2017-10-12 20:57:02 -0500

    commit 8
    se elimina una clase sobrante y se crean e implementan las clases cliente y productos

--- src/Proyecto2Corte/Cliente.java
+++
@@ -1,9 +0,0 @@
-package Proyecto2Corte;
-
-public class Cliente {
-
-	public Cliente() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- src/Proyecto2Corte/Clientes.java
+++ src/Proyecto2Corte/Clientes.java
@@ -1,9 +1,63 @@
 package Proyecto2Corte;
 
 public class Clientes {
-
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private int cedula;
+	private String empresa;
+	private float sueldo;
+	
 	public Clientes() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public float getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
 	}
 
 }
