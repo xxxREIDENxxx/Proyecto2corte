d9f09082e84a7f46f7a64c47b9b1306263240aa3
xxxREIDENxxx <reiden-3@hotmail.com>
2017-10-12 20:43:31 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/Proyecto2Corte/Proyecto2Corte.java
+++ src/Proyecto2Corte/Proyecto2Corte.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package Proyecto2Corte;
 
 public class Proyecto2Corte {
@@ -8,3 +9,15 @@ public class Proyecto2Corte {
 	}
 
 }
+=======
+package Proyecto2Corte;
+
+public class Proyecto2Corte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+>>>>>>> Stashed changes
--- src/Proyecto2Corte/VentanaPrincipal.java
+++ src/Proyecto2Corte/VentanaPrincipal.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package Proyecto2Corte;
 
 public class VentanaPrincipal {
@@ -7,3 +8,14 @@ public class VentanaPrincipal {
 	}
 
 }
+=======
+package Proyecto2Corte;
+
+public class VentanaPrincipal {
+
+	public VentanaPrincipal() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+>>>>>>> Stashed changes
